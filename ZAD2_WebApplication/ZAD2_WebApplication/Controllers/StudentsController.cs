1c70b7f29eb0b24c3595ad830f338725c2ee04fe
s17975@pjwstk.edu.pl <s17975@pjwstk.edu.pl>
2020-03-28 20:48:12 +0100

    Zadanie 2 - Commit

---
+++ ZAD2_WebApplication/ZAD2_WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZAD2_WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Rudko, Rudko2";
+        }
+    }
+}
